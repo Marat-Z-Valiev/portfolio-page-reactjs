cc0f3616a803dcc802cca98982665d108935e312
Marat Valiev <mz.valiev@gmail.com>
2018-12-30 23:14:05 -0500

    Add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,25 @@
 import React from "react";
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap";
+import "../css/Header.css";
 
 class Header extends React.Component {
 	render() {
 		const linksMarkup = this.props.links.map((link, index) => {
 			return (
-				<li className="nav-item active" key={index}>
-					<a className="nav-link" href={link.link}>
-						{link.label}
-					</a>
+				<li className="menu-list-item" key={index}>
+					<a href={link.link} />
+					{link.label}
 				</li>
 			);
 		});
 		return (
-			<Navbar inverse collapseOnSelect>
-				<Navbar.Header>
-					<Navbar.Brand>
-						<a href="#brand">Marat Valiev</a>
-					</Navbar.Brand>
-					<Navbar.Toggle />
-				</Navbar.Header>
-				<Navbar.Collapse>
-					<Nav pullRight>
-						{linksMarkup}
-						{/* <NavItem></NavItem> */}
-						{/* <NavItem eventKey={2} href="#">
-							Link
-						</NavItem>
-						<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-							<MenuItem eventKey={3.1}>Action</MenuItem>
-							<MenuItem eventKey={3.2}>Another action</MenuItem>
-							<MenuItem eventKey={3.3}>Something else here</MenuItem>
-							<MenuItem divider />
-							<MenuItem eventKey={3.3}>Separated link</MenuItem>
-						</NavDropdown> */}
-					</Nav>
-					{/* <Nav pullRight>
-						<NavItem eventKey={1} href="#">
-							Link Right
-						</NavItem>
-						<NavItem eventKey={2} href="#">
-							Link Right
-						</NavItem>
-					</Nav> */}
-				</Navbar.Collapse>
-			</Navbar>
+			<div className="container">
+				<nav className="menu">
+					<h1>Marat Valiev</h1>
+					<div className="menu-right">
+						<ul className="menu-list">{linksMarkup}</ul>
+					</div>
+				</nav>
+			</div>
 		);
 	}
 }
